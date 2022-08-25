3310881b014acc0ea0b707657beac8d80295768f
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2022-08-25 16:39:16 -0300

    rainbow and black colors using buttons

--- script.js
+++ script.js
@@ -1,48 +1,85 @@
 const gridContainer = document.querySelector('.container');
 const gridSetBtn16 = document.querySelector('#small-grid');
 const gridSetBtn64 = document.querySelector('#large-grid');
-const randomColorBtn = document.querySelector('#random-color-btn');
+const rainbowColorBtn = document.querySelector('#rainbow');
+const blackColorBtn = document.querySelector('#black');
 const clearBtn = document.querySelector('#clear-btn');
 const rootCssVariables = document.querySelector(':root');
 
 let gridSize = 16;
+let pixels = gridContainer.childNodes;
+/* 
+Each brush color work as a 'state', with refers to the brush that is currently active.
+All the states available are:
+black -> standard black color;
+rainbow -> Each pixel is painted with a random color;
+shade -> Each pixel is painted with 10% of black color. Successive clicks adds another 10% until full black
+*/
+let state = 'black';
+
+
 
 for (let i = 0; i < gridSize*gridSize; i++) {
     const pixel = document.createElement('div');
     pixel.classList.toggle('grid-pixel');
+    pixel.classList.toggle('blank-pixel');
     gridContainer.appendChild(pixel);
 }
 
-let pixels = gridContainer.childNodes;
-
-
-
+// Add an event listener for each pixel to paint on click
 pixels.forEach((element) => element.addEventListener('click', paintPixel));
+
+// event listener for the features buttons
 clearBtn.addEventListener('click', clearGrid);
+rainbowColorBtn.addEventListener('click', changeState);
+blackColorBtn.addEventListener('click', changeState);
+
+
+
+function changeState() {
+    state = this.id;
+}
 
 function clearGrid() {
-    pixels.forEach((element) => element.classList.remove('painted'));
+    pixels.forEach((element) => element.style.backgroundColor = 'white');
 }
 
 function paintPixel(){
-    // randomPaintColor(this);
-    this.classList.add('painted');
+    // paint pixel according with the current state
+    let currentState = checkState();
+    switch (currentState) {
+        case 'black':
+            paintBlackColor(this);
+            // this.classList.add('black-paint');
+        break;
+
+        case 'rainbow':
+            paintRandomColor(this);
+        break;
+    }
+}
+
+function checkState(){
+    return state;
 }
 
 function generateRandomColor() {
     let red = Math.round((Math.random() * 0xff)).toString(16);
     let green = Math.round((Math.random() * 0xff)).toString(16);
     let blue = Math.round((Math.random() * 0xff)).toString(16);
-    // Don't allow white color, since color is the background
+
+    // Don't allow white color, since color it is the background
     while (`#${red}${green}${blue}` === '#ffffff') {
         return generateRandomColor();
     }
     return `#${red}${green}${blue}`;
 }
 
-function randomPaintColor(pixel) {
+function paintBlackColor(pixel){
+    pixel.style.backgroundColor = 'black';
+}
+
+function paintRandomColor(pixel) {
     let color = generateRandomColor();
-    if (!pixel.classList.contains('painted')) {
-        pixel.style.backgroundColor = color;
-    }
+    pixel.style.backgroundColor = color;
 }
